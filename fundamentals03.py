2b4dd84fb43a66060c369940f5e1db8838a360ed
= <=>
2023-03-08 22:27:10 +0530

    image crop functions

---
+++ fundamentals03.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("Resources/lambo.png")
+print(img.shape)
+
+imgResize = cv2.resize(img,(1000, 500))
+print(imgResize.shape)
+
+imgCropped = img[0:200, 200:500]
+
+cv2.imshow("Image", img)
+# cv2.imshow("imgResize", imgResize)
+cv2.imshow("imgCropped", imgCropped)
+
+
+cv2.waitKey(0)
