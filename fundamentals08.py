96fda9836666884f0e2826c1f716cbe7b11eb418
= <=>
2023-04-02 23:19:57 +0530

    shape recongniztion

--- fundamentals08.py
+++ fundamentals08.py
@@ -42,6 +42,21 @@ def getContours(img):
             # print(peri)
             approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
             print(len(approx))
+            objCor = len(approx)
+            x, y, w, h = cv2.boundingRect(approx)
+
+            if objCor == 3 : objectType = "Tri"
+            elif objCor == 4:
+                aspRatio = w/float(h)
+                if aspRatio > 0.95 and aspRatio < 1.05: objectType = "square"
+                else : objectType = "Rectangle"
+            elif objCor > 4 : objectType = "Circle"
+            else : objectType = "None"
+
+            cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 225, 0), 2)
+            cv2.putText(imgContour, objectType,
+                        (x + (w // 2) - 10, y + (h // 2) - 10), cv2.FONT_HERSHEY_COMPLEX, 0.5,
+                        (0, 0, 0), 2)
 
 path = "Resources/shapes.png"
 img = cv2.imread(path)
@@ -55,7 +70,7 @@ imgCanny = cv2.Canny(imgBlur, 50, 50)
 getContours(imgCanny)
 
 imgBlank = np.zeros_like(img)
-imgStack = stackImages(0.6,([img, imgGray, imgBlur],
+imgStack = stackImages(0.7,([img, imgGray, imgBlur],
                             [imgCanny, imgContour, imgBlank]))
 
 cv2.imshow("Stack", imgStack)
